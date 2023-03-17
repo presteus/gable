1fd0ad228a50f6327456a915d6e3479defba9bfc
TB <terry.boeuf@hotmail.fr>
2023-03-17 14:29:28 +0100

    feat: ajout DTO pour toutes les entitys

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ConflictException, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+import { ApiTags } from '@nestjs/swagger';
 
+
+
+@ApiTags('users')
 @Controller('users')
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+ /**
+   * Cache le mot de passe pour toute la classe UserController
+    */
+@UseInterceptors(
+  ClassSerializerInterceptor,
+)
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    const mailExist = await this.usersService.findOneByMail(
+      createUserDto.email
+    )
+    if (mailExist) {
+      throw new ConflictException('Ce mail est déjà enregistré.')
+    }
+
+    createUserDto.password = await bcrypt.hash( /* Crypte le mot de passe */
+      createUserDto.password,
+      10
+    )
+    const data = await this.usersService.create(/* Créé le nouvel utilisateur */
+      createUserDto
+    )
+    return {
+      message: `${createUserDto.email} a bien été enregistré`,
+      data: data
+    }
   }
+
+
+
+
 
   @Get()
   findAll() {
